27659decff23b843be2b62dd3c0b15a21a4c46bc
yakovelmaleh <87435007+yakovelmaleh@users.noreply.github.com>
2021-12-29 22:07:12 +0200

    asd

--- Hackaton/Server.py
+++ Hackaton/Server.py
@@ -4,109 +4,125 @@ import select
 import selectors
 import socket
 import struct
-import threading
+from threading import Thread
 import time
 
 
 class Server:
-    
+
     def __init__(self):
-        self.name="the Server connect with you\n what is your name?"
+        self.name = "the Server connect with you\n what is your name?"
         self.sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock_udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock_tcp.setblocking(True)
-        self.sock_tcp.bind(('192.168.56.1', 13118))
-        self.port=13118
-        self.player1_socket=None
-        self.player2_socket=None
-        self.players=0
-        self.solution=None
-
+        self.sock_tcp.bind(('127.0.0.1', 13118))
+        self.port = 13118
+        self.player1_socket = None
+        self.player2_socket = None
+        self.players = 0
+        self.solution = None
 
     def create_math(self):
-        sign=random.choice(["+","-"])
-        digits=[1,2,3,4,5,6,7,8,9]
+        sign = random.choice(["+", "-"])
+        digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         num1 = random.choice(digits)
         num2 = random.choice(digits)
-        if sign=="+":
-            while num1+num2>=10:
-                num2=random.choice(digits)
-            self.solution=num1+num2
+        if sign == "+":
+            while num1 + num2 >= 10:
+                num2 = random.choice(digits)
+            self.solution = num1 + num2
         else:
-            while num1+num2<0:
-                num2=random.choice(digits)
+            while num1 + num2 < 0:
+                num2 = random.choice(digits)
             self.solution = num1 - num2
-        return "How much is "+str(num1)+sign+str(num2)+"?"
-
-
-
+        return "How much is " + str(num1) + sign + str(num2) + "?"
 
     def start_game(self):
-        name1=self.player1_socket.recv(1024)
-        name2=self.player2_socket.recv(1024)
-        message="Welcome to Quick Maths: \nPlayer 1: "+ name1 +"\nPlayer 2 : " +name2+"\n==\n Please answer the following question as fast as you can:\n"
-        formula=self.create_math()
-        formula_in_bytes=formula.encode()
-        dec=message.encode()
-        self.player1_socket.sendto(dec)
-        self.player2_socket.sendto(dec)
-        sel=selectors.DefaultSelector()
-        #t1=threading.Thread(()-> self.player1_socket.sendto(formula_in_bytes))
-        #t2 = threading.Thread(()-> self.player2_socket.sendto(formula_in_bytes))
+        try:
+            name1 = self.player1_socket.recv(1024)
+            name2 = self.player2_socket.recv(1024)
+            message = "Welcome to Quick Maths: \nPlayer 1: " + name1 + "\nPlayer 2 : " + name2 + "\n==\n Please answer the following question as fast as you can:\n"
+            formula = self.create_math()
+            formula_in_bytes = formula.encode()
+            dec = message.encode()
+            self.player1_socket.sendto(dec)
+            self.player2_socket.sendto(dec)
+
+            time.sleep(10)
+            self.player1_socket.sendto(formula_in_bytes)
+            self.player2_socket.sendto(formula_in_bytes)
+
+            yaki, _, _ = select.select([self.player1_socket, self.player2_socket], [], [], 10)
+            if len(yaki) == 0:
+                write = "is draw! the right answer is" + str(self.solution)
+                self.player1_socket.sendto(write)
+                self.player2_socket.sendto(write)
+            else:
+                if yaki[0] == self.player1_socket:
+                    ans = self.player1_socket.recv(1024).decode()
+                    if str(self.solution) == ans:
+                        write = ("the winner is " + name1 + ", the loser is " + name2).encode()
+                        self.player1_socket.sendto(write)
+                        self.player2_socket.sendto(write)
+                    else:
+                        write = ("the winner is " + name2 + ", the loser is " + name1).encode()
+                        self.player1_socket.sendto(write)
+                        self.player2_socket.sendto(write)
+                else:
+                    ans = self.player2_socket.recv(1024).decode()
+                    if str(self.solution) == ans:
+                        write = "the winner is " + name2 + ", the loser is " + name1
+                        self.player1_socket.sendto(write)
+                        self.player2_socket.sendto(write)
+                    else:
+                        write = "the winner is " + name1 + ", the loser is " + name2
+                        self.player1_socket.sendto(write)
+                        self.player2_socket.sendto(write)
+        except Exception as e:
+            print(e)
+
         self.player1_socket
-        reads,_,_=select.select([self.player1_socket,self.player2_socket],[],[],10)
-        time.sleep(10)
-        t1.start()
-        t2.start()
-        if len(reads)==0:
-            #draw
-        elif reads[1].
+        reads, _, _ = select.select([self.player1_socket, self.player2_socket], [], [], 10)
+
         self.player1_socket.sendto(formula_in_bytes)
         self.player2_socket.sendto(formula_in_bytes)
 
-
-        t1=threading.Thread(target=self.solve, args=(self.player2_socket,message,formula))
-        t2=threading.Thread(target=self.solve, args=(self.player2_socket,message,formula))
-
-    def solve(self,player,formula):
+    def solve(self, player, formula):
         player.sendto(formula)
-
-
 
     def init_Broad_Cast(self):
         try:
-            broad_cast = ("255.255.255.255", 13117)
-            pre = struct.pack("L", 0xabcddcba)
-            offer = struct.pack("B", 0x2)
-            port = struct.pack("H", self.port)
-            massage = b''.join([pre, offer, port])
+            broad_cast = ("<broadcast>", 13117)
+            massage=struct.pack("!IBH",0xabcddcba,0x2,self.port)
 
-            while self.players<2:
+            while self.players < 2:
                 self.sock_udp.sendto(massage, broad_cast)
                 time.sleep(1)
         except Exception as e:
             print(e)
 
     def init_accept_thread(self):
-        while self.players<2:
+        while self.players < 2:
             self.sock_tcp.listen(2)
-            new_conn,address=self.sock_tcp.accept()
+            new_conn, address = self.sock_tcp.accept()
             with new_conn:
-                if self.players==0:
-                    self.player1_socket=new_conn
+                if self.players == 0:
+                    self.player1_socket = new_conn
                 else:
-                    self.player2_socket=new_conn
-                    self.start_game()
+                    self.player2_socket = new_conn
 
     def run(self):
-        self.init_server()
-        threading.Thread(self.init_Broad_Cast()).start()
-        threading.Thread(self.init_accept_thread()).start()
-
+        while True:
+            t1 = Thread(target=self.init_Broad_Cast)
+            t2 = Thread(target=self.init_accept_thread)
+            t1.start()
+            t2.start()
+            t1.join()
+            t2.join()
+            self.start_game()
 
 
 if __name__ == '__main__':
     Server().run()
-
--- Hackaton/client.py
+++ Hackaton/client.py
@@ -1,8 +1,14 @@
 import socket
 import struct
+import selectors
+import sys
 
+teamName=b"SorryException\n"
 
 class Client:
+    def __init__(self):
+        self.sock = None
+        self.sel=selectors.DefaultSelector()
 
     def start(self):
         print("Client Started, listening for offer requests...")
@@ -13,6 +19,7 @@ class Client:
         print("Client::looking_for_server")
         sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
         sock.bind(('',13117))
         massage, address=sock.recvfrom(1024) # need to be 7
         self.check_package(massage,address)
@@ -20,31 +27,35 @@ class Client:
 
     def check_package(self,message,address):
         print("Client::check_package")
-        pre=struct.unpack("L",message[0:4])[0]
-        type=struct.unpack("B",message[4:5])[0]
+
+        pre,type,port=struct.unpack("!IBH",message)
 
         if pre==0xabcddcba and type==0x02:
-            port=struct.unpack("H",message[5:7])[0]
-            print(port)
-            sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            sock.connect((address[0],port))
-            sock.sendall(b"this is sendall")
-            print("here i send")
+            self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            print((address[0],port))
+            self.sock.connect((address[0],port))
+            print("nitay")
+            self.sock.send(teamName)
+            self.game_mode()
+            
+            
 
 
         else:
-            print("aleks")
+            print("Error")
 
-
-
-
-
-
-
-    def connection_with_server(self):
-        pass
 
 
     def game_mode(self):
-        pass
+        print("Client::game_mode")
+        inChar=""
+        self.sel.register(fileobj=sys.stdin, events=selectors.EVENT_READ)
+        self.sock_tcp.setblocking(False)
+        self.sel.register(self.sock,events=selectors.EVENT_READ)
+        question=self.sock.recv(1024)
+        print(question)
+        
 
+
+if __name__ == '__main__':
+    Client().start()
